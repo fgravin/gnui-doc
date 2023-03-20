4e3d2fab47059c330d99b15486047f3314ba57d6
Florent gravin <florent.gravin@camptocamp.com>
2023-03-20 15:56:42 +0100

    Initial commit

---
+++ src/.vitepress/config.ts
@@ -0,0 +1,61 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/doc/site-config
+export default defineConfig({
+  title: 'Geonetwork-UI',
+  description: 'Official documentation',
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide', activeMatch: '/guide/' },
+      { text: 'Reference', link: '/doc', activeMatch: '/doc/' },
+    ],
+
+    sidebar: {
+      '/guide/': sidebarGuide(),
+      '/doc/': sidebarDoc(),
+    },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/geonetwork/geonetwork-ui' },
+    ],
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2020-present GeoNetwork',
+    },
+  },
+})
+
+function sidebarGuide() {
+  return [
+    {
+      text: 'Introduction',
+      items: [
+        { text: 'Why ?', link: 'guide/why' },
+        { text: 'Get started', link: 'guide/what' },
+      ],
+    },
+  ]
+}
+
+function sidebarDoc() {
+  return [
+    {
+      text: 'Get started',
+      items: [
+        { text: 'Create a new lib', link: 'doc/newlib' },
+        { text: 'Create a new app', link: 'doc/newapp' },
+      ],
+    },
+    {
+      text: 'Essentials',
+      items: [
+        { text: 'Architecture', link: 'doc/architecture' },
+        { text: 'Theming', link: 'doc/theme' },
+        { text: 'State', link: 'doc/state' },
+        { text: 'Web components', link: 'doc/wc' },
+      ],
+    },
+  ]
+}
---
+++ src/guide/what.md
@@ -0,0 +1,11 @@
+# What is geonetwork-ui ?
+
+Geonetwork-ui c’est un éco-système d’outils et de services dédiés à la création simplifiée d’interfaces, d’applications, de composants ou de sites web autour de la notion de catalogue, de données et de métadonnées.
+
+Le projet se décline en une suite de services
+
+- Connect
+- Prepare
+- Publish
+- Explore
+- Share
